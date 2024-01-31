7a61802a6712c5eb4e6e3306bdd35b72a92f1f31
Timilehin1998 <67716936+Timilehin1998@users.noreply.github.com>
2024-01-31 04:10:41 -0500

    Update player_dashboard.py

--- player_dashboard.py
+++ player_dashboard.py
@@ -31,6 +31,7 @@ import urllib.request, json
 from urllib.request import urlopen 
 import requests
 import tempfile
+from google.oauth2 import service_account
 
 
 
@@ -54,43 +55,23 @@ API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def Create_Service(client_secret_file, api_name, api_version, *scopes, prefix=''):
-	CLIENT_SECRET_FILE = client_secret_file
-	API_SERVICE_NAME = api_name
-	API_VERSION = api_version
-	SCOPES = [scope for scope in scopes[0]]
-	
-	cred = None
-	working_dir = os.getcwd()
-	token_dir = 'token files'
-	pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.pickle'
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
 
-	### Check if token dir exists first, if not, create the folder
-	if not os.path.exists(os.path.join(working_dir, token_dir)):
-		os.mkdir(os.path.join(working_dir, token_dir))
+    # Load service account credentials from the JSON file
+    credentials = service_account.Credentials.from_service_account_file(CLIENT_SECRET_FILE, scopes=SCOPES)
 
-	if os.path.exists(os.path.join(working_dir, token_dir, pickle_file)):
-		with open(os.path.join(working_dir, token_dir, pickle_file), 'rb') as token:
-			cred = pickle.load(token)
-
-	if not cred or not cred.valid:
-		if cred and cred.expired and cred.refresh_token:
-			cred.refresh(Request())
-		else:
-			flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-			cred = flow.run_local_server()
-
-		with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
-			pickle.dump(cred, token)
-
-	try:
-		service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
-		print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
-		return service
-	except Exception as e:
-		print(e)
-		print(f'Failed to create service instance for {API_SERVICE_NAME}')
-		os.remove(os.path.join(working_dir, token_dir, pickle_file))
-		return None
+    try:
+        # Build the service using the service account credentials
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+        print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+        print(f'Failed to create service instance for {API_SERVICE_NAME}')
+        return None
 
 
 
